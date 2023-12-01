be419499a3b9ce884fc791d21c9d70c4bcb1c9b8
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-12-01 12:33:18 +0100

    Complete day 1 part 1

--- puzzles/1_1.go
+++ puzzles/1_1.go
@@ -2,11 +2,59 @@ package puzzles
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
+func get_first_digit(s string) (int, bool) {
+	for _, character := range s {
+		if unicode.IsDigit(character) {
+			number, err := strconv.Atoi(string(character))
+			if err != nil {
+				panic(err)
+			}
+
+			return number, true
+		}
+	}
+
+	return 0, false
+}
+
+func reverse(s string) string {
+	reversed_string := ""
+	for _, character := range s {
+		reversed_string = string(character) + reversed_string
+	}
+	return reversed_string
+}
+
+func get_last_digit(s string) (int, bool) {
+	return get_first_digit(reverse(s))
+}
+
 func trebuchet() any {
-	fmt.Println("Trebuchet")
-	return 0
+	input, err := os.ReadFile("resources/1_1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	sum := 0
+	for _, line := range strings.Split(string(input), "\n") {
+		first_digit, _ := get_first_digit(line)
+		last_digit, _ := get_last_digit(line)
+
+		number, err := strconv.Atoi(fmt.Sprintf("%d%d", first_digit, last_digit))
+		if err != nil {
+			panic(err)
+		}
+
+		sum += number
+	}
+
+	return sum
 }
 
 func init() {
